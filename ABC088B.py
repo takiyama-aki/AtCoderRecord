4cfe7ecf570267a5231d83aae02b49c85f63daff
akihito <takiyamaakihito@gmail.com>
2024-06-14 23:43:55 +0900

    ABC088B

---
+++ ABC088B.py
@@ -0,0 +1,19 @@
+# ABC088B - Card Game for Two
+
+card_num = int(input())
+card_list = sorted(map(int, input().split()), reverse=True)
+
+alicePoint = 0
+bobPoint = 0
+flg = True # True: alice's turn, False: bob's turn
+
+for i in range(card_num):
+
+    if flg:
+        alicePoint += card_list.pop(0)
+        flg = False
+    else:
+        bobPoint += card_list.pop(0)
+        flg = True
+
+print(alicePoint-bobPoint)
---
+++ readme.md
@@ -0,0 +1,8 @@
+# 標準入力
+input.txtに標準入力のテキストを記述
+
+# 実行
+main.py 実行時、input.txtをコマンドライン引数に渡して実行する
+```
+python hoge.py < input.txt
+```
