e7592d7025efd9b9c4ccf4afafb47e2b0b076a8a
akihito <takiyamaakihito@gmail.com>
2024-06-20 22:24:40 +0900

    自信あったのに

--- ABC096C.py
+++ ABC096C.py
@@ -4,9 +4,15 @@ canvas = [list(input()) for _ in range(height)]
 
 # 黒色の上下左右が白色の絵は描けない
 
+ok_flg = True
+# 1マスずつ見ていく
 for y in range(height):
     for x in range(width):
+        # 見てるマスが黒色だったら
         if canvas[y][x] == "#":
+            count = 0
+
+            # 上下左右のマスを見ていく
             for dx in range(-1,2):
                 for dy in range(-1,2):
                     if dx == 0 and dy == 0:
@@ -19,4 +25,13 @@ for y in range(height):
 
                     if xx >= 0 and xx < width and yy >= 0 and yy < height:
                         if xx == "#" or yy == "#":
-                            # この黒マスはokだから次の黒マスへ
+                            # この黒マスはok
+                            count += 1
+                    
+            if count == 0:
+                print("No")
+                exit()
+
+print("Yes")
+
+
